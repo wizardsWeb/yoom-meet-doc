fa897094dc0c4a8e422e56ccf8d275bf0c24f67a
Daksh Jain <dakshcjain@gmail.com>
2024-11-26 17:36:34 +0530

    integrated cypress

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,17 +1,17 @@
 import MeetingTypeList from '@/components/MeetingTypeList';
 
 const Home = () => {
-  const now = new Date();
+    const now = new Date();
 
-  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
-
+    const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
+    const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
+    const nextTime = time > '12:00 PM' ? '12:00 PM' : '6:00 PM';
   return (
     <section className="flex size-full flex-col gap-5 text-white">
       <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
         <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
           <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming Meeting at: 12:30 PM
+            {nextTime}
           </h2>
           <div className="flex flex-col gap-2">
             <h1 className="text-4xl font-extrabold lg:text-7xl">{time}</h1>
